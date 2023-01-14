1543e76c45a8ceebdcc437e0474c937638dce2da
dviluk <dviluk@outlook.jp>
2023-01-14 14:17:23 -0600

    wip

--- app/Http/Controllers/API/V1/Admin/DifficultiesController.php
+++ app/Http/Controllers/API/V1/Admin/DifficultiesController.php
@@ -6,6 +6,39 @@ use App\Http\Controllers\CRUDController;
 use App\Http\Resources\V1\DifficultyResource;
 use App\Repositories\V1\DifficultiesRepository;
 
+/**
+ * @OA\Get(
+ *  path="/api/v1/difficulties",
+ *  description="Difficulties",
+ *  @OA\Response(
+ *      response=200, 
+ *      description="List of categories",
+ *      @OA\JsonContent(
+ *        type="string",
+ *        required={"name"},
+ *        @OA\Property(property="name", type="string")
+ *      )
+ *  )
+ * )
+ * @OA\GET(
+ *   path="/api/v1/difficulties/{id}",
+ *   description="Item",
+ *   @OA\Parameter(
+ *     name="id",
+ *     in="path",
+ *     required=true,
+ *   ),
+ *   @OA\Response(
+ *     response=200,
+ *     description="Item",
+ *     @OA\JsonContent(
+ *       type="string",
+ *       required={"name"},
+ *       @OA\Property(property="name", type="string")
+ *     )
+ *   )
+ * )
+ */
 class DifficultiesController extends CRUDController
 {
     /**
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *   security={{"bearerAuth": {}}}
+ * )
+ * @OA\SecurityScheme(
+ *   securityScheme="bearerAuth",
+ *   type="http",
+ *   scheme="bearer"
+ * )
+ * @OA\Info(
+ *   title="My first API",
+ *   version="1.0.0",
+ *   @OA\Contact(
+ *     email="support@example.com"
+ *   )
+ * )
+ *  */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
